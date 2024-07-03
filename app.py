d73501177e3e30ddaf6a8dc7aa08960d50ff6d89
Dominique <122916223+mbdominique@users.noreply.github.com>
2024-07-03 16:36:34 +0000

    just tring

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_behind_proxy import FlaskBehindProxy
+
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app)  ## add this line
+app.config['SECRET_KEY'] ='d162acea47fad10761eb2c2b2d5d56a3'
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+    
